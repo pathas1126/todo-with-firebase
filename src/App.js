f646e707721d8f8021d1cc758a8a9d6fc26a33e5
pathas <pathas@naver.com>
2020-04-10 00:43:34 +0900

    #10 2:17

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useRef } from "react";
+import { TaskAdd, TaskDisplay } from "./components";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const idRef = useRef(1);
+  const [modify, setModify] = useState(false);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    if (task !== "") {
+      const todo = { todo: task, id: idRef.current };
+      setTasks((prevTasks) => tasks.concat(todo));
+      setTask("");
+      idRef.current += 1;
+    }
+  };
+
+  const onChangeHandler = (e) => {
+    setTask(e.target.value);
+  };
+
+  const removeHandler = (id) => {
+    setTasks((prevTasks) => tasks.filter((task) => task.id !== id));
+  };
+
+  const modifyHandler = (id) => {
+    return (e) => {
+      setTask("");
+      setModify(true);
+      e.target.innerText = modify ? "수정" : "완료";
+      if (task !== "" && modify) {
+        setTasks((prevTasks) =>
+          tasks.map((taskOne) =>
+            taskOne.id === id ? { ...taskOne, todo: task } : taskOne
+          )
+        );
+        setTask("");
+        setModify(false);
+      }
+    };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskAdd
+        task={task}
+        onClickHandler={onClickHandler}
+        onChangeHandler={onChangeHandler}
+        modify={modify}
+      />
+      <TaskDisplay
+        tasks={tasks}
+        removeHandler={removeHandler}
+        modifyHandler={modifyHandler}
+      />
     </div>
   );
 }
